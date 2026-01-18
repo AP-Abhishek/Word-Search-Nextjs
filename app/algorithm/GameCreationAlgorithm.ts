60e75977643473459bf6013c0c28982bcc59b7bf
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-18 21:28:59 +0530

    fix: fixed algorithm's bug

--- app/algorithm/GameCreationAlgorithm.ts
+++ app/algorithm/GameCreationAlgorithm.ts
@@ -8,17 +8,18 @@ export default function GameCreationAlgorithm({ gridSize, words }: GameCreationA
   const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const grid = Array.from({length: gridSize}, () => new Array(gridSize).fill(null));
   const sortedWords = words.sort((a, b) => b.length - a.length);
+  const finalWords = sortedWords.map(word => word.toUpperCase());
   const mappingPositions = [[1, 0], [0, 1], [1, 1], [-1, 0], [0, -1], [-1, -1]];
 
-  for (const word of sortedWords) {
+  for (const word of finalWords) {
     let placed = false;
     let attempts = 0;
 
     while (!placed && attempts < 100) {
       let direction = mappingPositions[Math.floor(Math.random() * mappingPositions.length)];
 
-      let startX = Math.floor(Math.random() * gridSize - 1);
-      let startY = Math.floor(Math.random() * gridSize - 1);
+      let startX = Math.floor(Math.random() * gridSize);
+      let startY = Math.floor(Math.random() * gridSize);
 
       let endX = startX + (word.length - 1) * direction[0];
       let endY = startY + (word.length - 1) * direction[1];
@@ -29,7 +30,7 @@ export default function GameCreationAlgorithm({ gridSize, words }: GameCreationA
           let currX = startX + (i * direction[0]);
           let currY = startY + (i * direction[1]);
 
-          let existingLetter = grid[currX][currY];
+          let existingLetter = grid[currY][currX];
           if (existingLetter !== null && existingLetter !== word[i]) {
             canPlace = false;
             break;
@@ -44,15 +45,15 @@ export default function GameCreationAlgorithm({ gridSize, words }: GameCreationA
       }
       attempts++;
     }
-    
-    for (let col of grid) {
-      for (let cell of col) {
-        if (cell == null) {
-          cell = alphabets[Math.floor(Math.random() * alphabets.length)];
-        }
+  }
+
+  for (let i=0 ; i<grid.length ; i++) {
+    for (let j=0 ; j<grid[i].length ; j++) {
+      if (grid[i][j] === null) {
+        grid[i][j] = alphabets[Math.floor(Math.random() * alphabets.length)];
       }
     }
   }
   
-  return grid;
+  return grid as string[][];
 }
